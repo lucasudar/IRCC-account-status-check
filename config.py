002ea6cc764aa52d14e34a8550ca5e69f506ca32
_Lucas_ <lucasudar@gmail.com>
2021-07-02 10:26:12 +0300

    added functionality for checking status

---
+++ config.py
@@ -0,0 +1,22 @@
+# Your login
+username_token = 'REPLACE_ME'
+# Your password
+password_token = 'REPLACE_ME'
+# Security question №1
+question_one = 'REPLACE_ME'
+# Security answer №1
+answer_one = 'REPLACE_ME'
+# Security question №2
+question_two = 'REPLACE_ME'
+# Security answer №2
+answer_two = 'REPLACE_ME'
+# Security question №3
+question_three = 'REPLACE_ME'
+# Security answer №3
+answer_three = 'REPLACE_ME'
+# Security question №4
+question_four = 'REPLACE_ME'
+# Security answer №4
+answer_four = 'REPLACE_ME'
+# Telegram token
+token_telegram = 'REPLACE_ME'
---
+++ main.py
@@ -0,0 +1,53 @@
+from selenium import webdriver
+import time
+from config import username_token, password_token, question_one, answer_one, question_two, answer_two, question_three, answer_three, question_four, answer_four
+
+PATH = r"C:\Users\_Lucas_\Downloads\chromedriver.exe"
+driver = webdriver.Chrome(PATH)
+
+
+driver.get("https://onlineservices-servicesenligne-cic.fjgc-gccf.gc.ca/mycic/gccf?lang=eng&idp=gckey&svc=/mycic/start")
+
+username = driver.find_element_by_id('token1')
+username.send_keys(username_token)
+
+password = driver.find_element_by_id('token2')
+password.send_keys(password_token)
+time.sleep(1)
+
+def buttonClick():
+    signIN = driver.find_element_by_class_name('btn.btn-primary')
+    signIN.click()
+    time.sleep(1)
+
+buttonClick()
+buttonClick()
+buttonClick()
+
+answer = driver.find_element_by_id('answer')
+
+if question_one in driver.page_source:
+    answer.send_keys(answer_one)
+    buttonClick()
+
+elif question_two in driver.page_source:
+    answer.send_keys(answer_two)
+    buttonClick()
+
+elif question_three in driver.page_source:
+    answer.send_keys(answer_three)
+    buttonClick()
+
+elif question_four in driver.page_source:
+    answer.send_keys(answer_four)
+    buttonClick()
+
+result = driver.find_element_by_xpath('/html/body/div[1]/main/div[1]/div/table/tbody/tr[1]/td[6]').text
+
+if result == 'Read':
+    print('Current status' + ': ' + 'Nothing new, try again later.')
+else:
+    print('Current status' + ': ' + 'Update!')
+
+time.sleep(10)
+driver.quit()
